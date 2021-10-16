1a3ec6b7caf056364a2fd51d2453422b3762ba39
ruslanpynzar <biopyn@yandex.ru>
2021-10-16 09:57:08 +0300

    HW7

---
+++ server/src/main/java/test/Main.java
@@ -0,0 +1,20 @@
+package test;
+
+import test.myTesting.TestsHandler;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Task1
+         */
+        ClassForTesting classForTesting = new ClassForTesting();
+        TestsHandler.start(classForTesting.getClass());
+
+        /**
+         * Task2
+         */
+        System.out.println();
+        ClassExplorer.outClassInfo(String.class);
+    }
+}
+
